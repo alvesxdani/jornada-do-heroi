03b74a980ec17461c9e622a1f1328df3eb7cd39f
alvesxdani <dani.alvesm22@gmail.com>
2023-10-28 11:51:40 -0300

    :construction: add store and create user interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
-import { Button } from '@mui/material';
-import './globals.css';
+"use client";
+
+import "./globals.css";
+import { useDataStore } from "./store/useDataStore";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <>
-    <h1 className='text-red'>Teste</h1>
-    </>
-  )
+  const { heroes, powerType, isLoading, error, fetchData, togglePower } =
+    useDataStore();
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  if (isLoading) <p>Loading...</p>;
+
+  if (heroes)
+    return (
+      <>
+        {heroes.map((item) => (
+          <ul key={item.id} className="border mb-5">
+            <li>{item.id}</li>
+            <li>{item.name}</li>
+          </ul>
+        ))}
+      </>
+    );
 }
--- src/app/store/useDataStore.tsx
+++ src/app/store/useDataStore.tsx
@@ -1,11 +1,37 @@
+// Imports
 import { create } from "zustand";
+import axios from "axios";
+import { IDataStore } from "../interfaces/interfaces";
 
-interface IDataStore {
-    data: Object[]
-}
+const api = "http://homologacao3.azapfy.com.br/api/ps/metahumans";
 
-export const useDataStore = create<IDataStore>((set) => {
-    return {
-        data: [],
+export const useDataStore = create<IDataStore>((set) => ({
+  heroes: [],
+  powerType: [],
+  isLoading: false,
+  error: null,
+
+  fetchData: async () => {
+    try {
+      set({ isLoading: true });
+      const response = await axios.get(api);
+      set({
+        heroes: response.data,
+        isLoading: true,
+      });
+    } catch (error) {
+      set({ error });
+    } finally {
+        set({ isLoading: false })
     }
-})
+  },
+
+  // Function to filter heroes
+  togglePower: (power: string) => {
+    set((state) => ({
+        powerType: state.powerType.includes(power) ?
+            state.powerType.filter((item) => item !== power) :
+                [...state.powerType, power]
+    }))
+  },
+}));
